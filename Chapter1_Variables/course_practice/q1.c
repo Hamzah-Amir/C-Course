c0177fdbcf610581f815b85363219a5ac142c514
Hamza <hamzaamir7651@gmail.com>
2026-09-12 18:45:18 +0500

    chapter 1 practice set

---
+++ Chapter1_Variables/course_practice/q1.c
@@ -0,0 +1,26 @@
+/* 
+Calculate area of rectangle using:
+a) hard coded values
+b) input by user
+*/
+
+
+#include <stdio.h>
+int main() {
+
+    // Solution A:
+    int area_a, length_a, width_a;
+    length_a = 12;
+    width_a = 4;
+    area_a = length_a * width_a;
+    printf("The area of rectangle by hard-coded values is is: %d", area_a);
+
+    // Solution b
+    int area_b, length_b, width_b;
+    printf("\nEnter Length: ");
+    scanf("%d", &length_b);
+    printf("Enter Width: ");
+    scanf("%d", &width_b);
+    area_b = length_b * width_b;
+    printf("Area of rectangle by input values is: %d", area_b);
+}
