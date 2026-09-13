eff28bd24faa86027f54e2bbb04a60db96e3171f
Hamza <hamzaamir7651@gmail.com>
2026-09-13 18:51:52 +0500

    Chapter 3 arithmetic operators

---
+++ Chapter2_Instruction_operators/arithmetic.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10,  b = 13,  c = 3,  d = 7;
+    int e = a + b;
+    printf("The sum of %d + %d is %d\n", a, b, e);
+    // Modulus operator is used to get remainder
+    printf("The remainder when b is divided by d is %d", b%d);
+    return 0;
+}
